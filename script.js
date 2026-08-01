63a460875c948894165947a01149b1bf6297dee4
Khadija Rafaqat <khadijarafaqat2@gmail.com>
2026-08-01 12:33:15 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const questions = [
+  {
+    question: "Capital of Pakistan?",
+    options: [
+      "Lahore",
+      "Islamabad",
+      "Karachi",
+      "Multan"
+    ],
+    answer: "Islamabad"
+  },
+
+  {
+    question: "2 + 2 = ?",
+    options: ["2","4","6","8"],
+    answer: "4"
+  }
+];
